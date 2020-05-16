40f645a5ddf49fe575d888f44d7f6a3b04534dc0
neruneru127 <65465508+neruneru127@users.noreply.github.com>
2020-05-17 04:45:53 +0900

    初回コミット Form,Common,Defineクラスを作成

---
+++ CommandEditor/CommandEditor/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace CommandEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新規プロジェクトNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.プロジェクトを保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.名前を付けて保存AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.データパック出力ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルFToolStripMenuItem
+            // 
+            this.ファイルFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新規プロジェクトNToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.プロジェクトを保存ToolStripMenuItem,
+            this.名前を付けて保存AToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.データパック出力ToolStripMenuItem});
+            this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.ファイルFToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // 新規プロジェクトNToolStripMenuItem
+            // 
+            this.新規プロジェクトNToolStripMenuItem.Name = "新規プロジェクトNToolStripMenuItem";
+            this.新規プロジェクトNToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.新規プロジェクトNToolStripMenuItem.Text = "新規プロジェクト(&N)...";
+            this.新規プロジェクトNToolStripMenuItem.Click += new System.EventHandler(this.新規プロジェクトNToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(183, 6);
+            // 
+            // プロジェクトを保存ToolStripMenuItem
+            // 
+            this.プロジェクトを保存ToolStripMenuItem.Name = "プロジェクトを保存ToolStripMenuItem";
+            this.プロジェクトを保存ToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.プロジェクトを保存ToolStripMenuItem.Text = "プロジェクトを保存(&S)";
+            // 
+            // 名前を付けて保存AToolStripMenuItem
+            // 
+            this.名前を付けて保存AToolStripMenuItem.Name = "名前を付けて保存AToolStripMenuItem";
+            this.名前を付けて保存AToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.名前を付けて保存AToolStripMenuItem.Text = "名前を付けて保存(A)...";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(183, 6);
+            // 
+            // データパック出力ToolStripMenuItem
+            // 
+            this.データパック出力ToolStripMenuItem.Name = "データパック出力ToolStripMenuItem";
+            this.データパック出力ToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.データパック出力ToolStripMenuItem.Text = "データパック出力";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "NelCommandEditor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 新規プロジェクトNToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem プロジェクトを保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 名前を付けて保存AToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem データパック出力ToolStripMenuItem;
+    }
+}
+
